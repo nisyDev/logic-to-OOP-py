ff05e4954237a35093be891d9d7754cd23f0dd8e
Denise N. M. <denisenmotta2002@gmail.com>
2026-05-05 12:36:43 -0300

    Delete FUNDAMENTOS directory

--- FUNDAMENTOS/Intermed/action.py
+++
@@ -1,9 +0,0 @@
-from person import Person
-#se eu quero criar um objeto a partir da minha classe, eu vou instanciar esse objeto
-
-p1 = Person('Luiz', 29) #instância
-p1.eat('apple')
-p1.speak('POO')
-p1.stop_eating()
-p1.speak('POO')
-
